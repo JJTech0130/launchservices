9b47f2deee6b500f318f0a0e653e250814fae3b8
JJTech0130 <jjtech@jjtech.dev>
2024-11-09 20:56:37 -0500

    small objc changes

--- objc/test.m
+++ objc/test.m
@@ -1,4 +1,5 @@
 #import <Foundation/Foundation.h>
+#import <objc/runtime.h>
 
 extern id _CSStoreCreateWithURL(NSURL *url, NSError **error);
 extern id CSStoreCreateMutable(NSError **error);
@@ -11,7 +12,14 @@ extern id _CSStoreEnumerateTables(id store, EnumerateBlock block);
 
 extern int _CSStoreGetArrayTable(id store);
 extern id _CSStoreCopyDebugDescriptionOfTable(id store, int table, bool someOption);
-extern void* CSStoreGetUnit(id store, int table, char *unit, void *arg4);
+extern void* CSStoreGetUnit(id store, int table, int unit, void *arg4);
+
+extern int CSStringBindingStoreInit(id store, NSString *name, int arg3, int arg4, void *out);
+extern int _CSStringBindingGetBindings(id store, int table, int unit);
+
+@interface CSStore : NSObject
+- (id)mutableCopyWithZone:(NSZone *)zone error:(NSError **)error;
+@end
 
 int main() {
     NSURL *url = [NSURL URLWithString:@"file:///tmp/csstore"];
@@ -27,6 +35,8 @@ int main() {
     NSURL *existingURL = [NSURL URLWithString:@"file:///Users/jjtech/Downloads/com.apple.LaunchServices-3027-v2.csstore"];
     //NSURL *existingURL = [NSURL URLWithString:@"file:///private/var/folders/4n/tqjh0y5n45s_lr7ylncnqsy40000gn/0/com.apple.LaunchServices.dv/com.apple.LaunchServices-5019-v2.csstore"];
     id existingStore = _CSStoreCreateWithURL(existingURL, &error);
+    // Force cast
+    existingStore = [(CSStore *)existingStore mutableCopyWithZone:nil error:nil];
     if (!existingStore) {
         NSLog(@"error: %@", error);
         return 1;
@@ -44,21 +54,36 @@ int main() {
     }
     NSLog(@"arrayTable: %d", arrayTable);
 
-    id debugDescription = _CSStoreCopyDebugDescriptionOfTable(existingStore, 80, false);
+    id debugDescription = _CSStoreCopyDebugDescriptionOfTable(existingStore, 60, false);
     if (!debugDescription) {
         NSLog(@"error: %@", error);
         return 1;
     }
-    NSLog(@"debugDescription: %@", debugDescription);
+    NSLog(@"debugDescription: %@", [debugDescription string]);
 
-    void *arg4 = NULL;
-    void *unit = CSStoreGetUnit(existingStore, 80, "", &arg4);
-    NSLog(@"arg4: %p, unit: %p", arg4, unit);
-    if (!unit) {
-        NSLog(@"error: %@", error);
+    void *out1 = NULL;
+    int result = CSStringBindingStoreInit(existingStore, @"UTIBinding", 4, 1, &out1);
+    if (result) {
+        NSLog(@"error: %d", result);
         return 1;
     }
-    NSLog(@"unit: %@", unit);
+    
+
+
+    // int bindings = _CSStringBindingGetBindings(existingStore, 60, 0);
+    // if (!bindings) {
+    //     NSLog(@"error: %@", error);
+    //     return 1;
+    // }
+    // NSLog(@"bindings: %d", bindings);   
+    //void *arg4 = NULL;
+    //void *unit = CSStoreGetUnit(existingStore, 80, "", &arg4);
+    //NSLog(@"arg4: %p, unit: %p", arg4, unit);
+    //if (!unit) {
+    ///    NSLog(@"error: %@", error);
+    //    return 1;
+    //}
+    //NSLog(@"unit: %@", unit);
 
 
     
